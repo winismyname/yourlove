fc966f9eb82bc91cdc2c20434d2bbcf5740973a6
winismyname <95560334+winismyname@users.noreply.github.com>
2021-12-05 13:20:05 +0700

    app my website

---
+++ config.js
@@ -0,0 +1,18 @@
+// 1. Chỉnh sửa các dòng chữ theo mục đích của bạn
+const CONFIG = {
+    introTitle: '🖐🖐🖐 Hee loo ! 🖐🖐🖐',
+    introDesc: `WIN có điều này muốn hỏi Em, Em nhớ phải trả lời thật lòng nha`,
+    btnIntro: 'VÂNG',
+    title: 'WIN biết Em thích WIN phải không phải không ? 😙',
+    desc: 'Em không trả lời hoặc thoát ra, tức là Em thích WIN đó nha :> ',
+    btnYes: 'Có, em thật sự thích WIN 💖💖💖',
+    btnNo: '💔 WIN mơ à ! 💔 ',
+    question:'Nói cho WIn biết đi vì sao Em thích WIN 😊😊',
+    reasonPlaceholder: 'Lý do',
+    btnReply: 'Gởi ! 💌💌',
+    reply: 'Tại vì yêu WIN vãi lồn 😊😊',
+    mess: 'WIN biết mà, WIn cũng thích em lắm 🥰. Love you 300.000 😘😘',
+    messDesc: '💬💬 Nhắn tin cho WIN nha. 💬💬',
+    btnAccept: 'Dạ vâng 💖',
+    messLink: 'https://m.me/toan.thilyhuu'
+}
